3b82dba8bf77f143172cfd563aab7a2e51641320
Lilla <korosililla@gmail.com>
2019-11-25 15:51:21 +0100

    added rest of the buttons

--- src/client/components/SidebarMenu/SidebarMenu.component.js
+++ src/client/components/SidebarMenu/SidebarMenu.component.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Icon from './Icon';
 import searchIcon from './icons/search.png';
+import projectIcon from './icons/project.png';
+import emailIcon from './icons/email.png';
+import commentsIcon from './icons/comments.png';
+import shareIcon from './icons/share.png';
+import settingsIcon from './icons/settings.png';
 
 const SidebarMenu = () => {
-    const search = "fas fa-search";
-    console.log(searchIcon);
     return (
         <div className="side-menu">
-            {/* <h1>icon</h1> */}
-            {/* <img src={searchIcon} alt="search" /> */}
             <Icon iconSrc={searchIcon} iconAlt="search" /> 
+            <Icon iconSrc={projectIcon} iconAlt="project" /> 
+            <Icon iconSrc={emailIcon} iconAlt="email" /> 
+            <Icon iconSrc={commentsIcon} iconAlt="comments" /> 
+            <Icon iconSrc={shareIcon} iconAlt="share" /> 
+            <Icon iconSrc={settingsIcon} iconAlt="settings" /> 
         </div>
     )
 }
