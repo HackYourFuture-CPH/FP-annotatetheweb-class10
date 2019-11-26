7ac007d393f0bcbfbbf26d98435f8dad4f36df8b
Lilla <korosililla@gmail.com>
2019-11-26 13:22:55 +0100

    menubar ready and fixed to bottom

--- src/client/components/SidebarMenu/SidebarMenu.component.js
+++ src/client/components/SidebarMenu/SidebarMenu.component.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import './sidebarMenu.css';
-import Icon from './Icon';
-import searchIcon from './icons/search.png';
-import projectIcon from './icons/project.png';
-import emailIcon from './icons/email.png';
-import commentsIcon from './icons/comments.png';
-import shareIcon from './icons/share.png';
-import settingsIcon from './icons/settings.png';
+import './SidebarMenu.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faCopy,
+  faEnvelope,
+  faCommentAlt,
+  faShareAlt,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
 
 const SidebarMenu = () => {
-    return (
-        <div className="side-menu">
-            <Icon iconSrc={searchIcon} iconAlt="search" imgClass="icon-image active"/> 
-            <Icon iconSrc={projectIcon} iconAlt="project" imgClass="icon-image inactive" /> 
-            <Icon iconSrc={emailIcon} iconAlt="email" imgClass="icon-image inactive"/> 
-            <Icon iconSrc={commentsIcon} iconAlt="comments" imgClass="icon-image active" /> 
-            <Icon iconSrc={shareIcon} iconAlt="share" imgClass="icon-image active"/> 
-            <Icon iconSrc={settingsIcon} iconAlt="settings" imgClass="icon-image inactive" /> 
-        </div>
-    )
-}
+  return (
+    <div className="wrapper">
+      <div className="side-menu">
+        <button>
+          <FontAwesomeIcon icon={faSearch} size="lg" className="invert-color" />
+        </button>
+        <button className="inactive">
+          <FontAwesomeIcon icon={faCopy} size="lg" className="invert-color " />
+        </button>
+        <button className="inactive">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            size="lg"
+            className="invert-color "
+          />
+        </button>
+        <button>
+          <FontAwesomeIcon
+            icon={faCommentAlt}
+            size="lg"
+            className="invert-color"
+          />
+        </button>
+        <button>
+          <FontAwesomeIcon
+            icon={faShareAlt}
+            size="lg"
+            className="invert-color"
+          />
+        </button>
+        <button className="inactive">
+          <FontAwesomeIcon icon={faCog} size="lg" className="invert-color" />
+        </button>
 
+      </div>
+    </div>
+  );
+};
 
 export default SidebarMenu;
--- src/client/components/SidebarMenu/SidebarMenu.story.js
+++ src/client/components/SidebarMenu/SidebarMenu.story.js
@@ -3,3 +3,4 @@ import { storiesOf } from "@storybook/react";
 import SidebarMenu from './SidebarMenu.component';
 
 storiesOf("SidebarMenu", module).add("mobile version", () => <SidebarMenu />)
+storiesOf("SidebarMenu", module).add("desktop version", () => <SidebarMenu />)
