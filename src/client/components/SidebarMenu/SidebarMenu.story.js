43fdbd97a3a19d0196c3c007c219873b44bd133e
Lilla <korosililla@gmail.com>
2019-11-27 11:50:56 +0100

    added responsive version

--- src/client/components/SidebarMenu/SidebarMenu.story.js
+++ src/client/components/SidebarMenu/SidebarMenu.story.js
@@ -3,8 +3,10 @@ import {storiesOf} from '@storybook/react';
 import SidebarMenu from './MobileVersion/SidebarMenu.component';
 import SidebarMenuDesktop
   from './DesktopVersion/SidebarMenuDesktop.component';
+import SidebarMenuResp from './responsive/SidebarMenuResp.component';
 
 storiesOf ('SidebarMenu', module).add ('mobile version', () => <SidebarMenu />);
 storiesOf ('SidebarMenu', module).add ('desktop version', () => (
   <SidebarMenuDesktop />
 ));
+storiesOf ('SidebarMenu', module).add('responsive', () => <SidebarMenuResp />)
---
+++ src/client/components/SidebarMenu/responsive/SidebarMenuResp.component.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faCopy,
+  faEnvelope,
+  faCommentAlt,
+  faShareAlt,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
+import './SidebarMenuResp.css';
+
+const SidebarMenuResp = () => {
+    return (
+        <div className="wrapper-resp">
+          <div className="side-menu-resp">
+            <button className="icon-width-resp light">
+              <FontAwesomeIcon
+                icon={faSearch}
+                size="lg"
+                className="invert-color"
+                fixedWidth
+              />
+            </button>
+            <button className="icon-width-resp light inactive">
+              <FontAwesomeIcon
+                icon={faCopy}
+                size="lg"
+                className="invert-color"
+                fixedWidth
+              />
+            </button>
+            <button className="icon-width-resp light inactive">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                size="lg"
+                className="invert-color "
+                fixedWidth
+              />
+            </button>
+            <button className="icon-width-resp light">
+              <FontAwesomeIcon
+                icon={faCommentAlt}
+                size="lg"
+                className="invert-color"
+                fixedWidth
+              />
+            </button>
+            <button className="icon-width-resp light">
+              <FontAwesomeIcon
+                icon={faShareAlt}
+                size="lg"
+                className="invert-color"
+                fixedWidth
+              />
+            </button>
+            <button className="icon-width-resp light inactive">
+              <FontAwesomeIcon
+                icon={faCog}
+                size="lg"
+                className="invert-color"
+                fixedWidth
+              />
+            </button>
+    
+          </div>
+        </div>
+      );
+}
+
+export default SidebarMenuResp;
