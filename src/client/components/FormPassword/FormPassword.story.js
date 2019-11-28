737fbdad5260a1a021e156feaed59a78f06498f6
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-11-28 15:29:38 +0100

    login form password field component with responsive feature

---
+++ src/client/components/FormPassword/FormPassword.story.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import FormPassword from './FormPassword.component';
+
+storiesOf('FormPassword',module).add('with password' ,() => <FormPassword/>)
