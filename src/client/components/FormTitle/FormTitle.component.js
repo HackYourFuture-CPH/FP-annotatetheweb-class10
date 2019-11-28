4fd8e977cb870b141b82a3ff921e4f9363f44e85
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-11-28 15:31:26 +0100

    login form title

---
+++ src/client/components/FormTitle/FormTitle.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './FormTitle.css';
+
+const FormTitle = (()=>{
+    
+return(
+
+        <div className='form-title'>Login to Annotate the web</div>
+       
+)
+})
+
+export default FormTitle;
