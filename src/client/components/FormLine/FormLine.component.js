6e3d71b025a45885d1af64420f45b11ab3d30a17
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-11-28 15:33:04 +0100

    login form line

---
+++ src/client/components/FormLine/FormLine.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './FormLine.css';
+
+const FormLine = (()=>{
+    
+return(
+
+        <div className='form-line'>Or</div>
+       
+)
+})
+
+export default FormLine;
