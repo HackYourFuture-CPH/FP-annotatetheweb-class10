584ffc25c3fcfd15eaf7840e280ed134bc7edf97
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-11-28 15:22:01 +0100

    Login Form Email Address component with responsive feature

---
+++ src/client/components/FormEmailAddress/FormEmailAddress.story.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import FormEmailAddress from './FormEmailAddress.component';
+
+storiesOf('FormEmailAddress',module).add('with textbox' ,() => <FormEmailAddress/>)
