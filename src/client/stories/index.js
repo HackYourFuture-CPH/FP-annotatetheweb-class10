dbe563e9d46b91ab7632c06060dc410083d3eb2c
Olha Afanasieva <afanasievaolja@gmail.com>
2019-11-30 10:39:55 +0100

    Delete unnecessarry code in the stories index

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -1,14 +1,1 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Button } from "@storybook/react/demo";
 import { ToggleButton } from "../components/ToggleButton/ToggleButton.story";
-
-storiesOf("Button", module)
-  .add("with text", () => <Button>Hello!</Button>)
-  .add("with emoji", () => (
-    <Button>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
