85087e5109710d9e8917e9ae635d56c402647f58
Olha Afanasieva <afanasievaolja@gmail.com>
2019-11-30 15:41:06 +0100

    ckeck if UrlInput does not have a second parameter onEnter

--- src/client/components/UrlInput/UrlInput.component.jsx
+++ src/client/components/UrlInput/UrlInput.component.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './UrlInput.css';
 
 const UrlInput = ({ placeholder, onEnter }) => {
-
     return (
         <input type="url" placeholder={placeholder} className="url-input"
             onKeyPress={event => {
-                if (event.key === 'Enter') {
+                if (event.key === 'Enter' && typeof onEnter === 'function') {
                     { onEnter(event.target.value) }
                 }
             }} />
