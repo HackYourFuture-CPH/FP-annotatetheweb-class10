045130ed898c5e2a76ddcf5a66e34ae139ee219c
Girmay <ink423103@yahoo.com>
2019-12-01 23:50:44 +0100

    style and props added

--- src/client/components/DotButton/DotButton.Component.jsx
+++ src/client/components/DotButton/DotButton.Component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import List from "../List/List.Component";
+//import List from "../List/List.Component";
 const DotButton = props => {
     return(        
         <ul className="btn-right" >
-            <List title={'...'}/>            
+            <button className="btn-right" onClick={props.onClickHandle} onBlur={props.onClickHandle}>
+                {props.title}</button>           
         </ul> 
     )        
 }
