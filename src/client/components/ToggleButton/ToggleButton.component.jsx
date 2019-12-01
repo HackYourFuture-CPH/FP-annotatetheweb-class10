d77bcc7149700a6d84f5f548c8a65515a460005a
Olha Afanasieva <afanasievaolja@gmail.com>
2019-12-01 23:06:04 +0100

    test for ToggleButton component, a checked value pass in a function

---
+++ src/client/components/ToggleButton/ToggleButton.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ToggleButton.css';
+
+const ToggleButton = ({ onChange, activeText, inactiveText }) => {
+    return (
+        <label className="switch">
+            <input type="checkbox" onChange={event => {
+                onChange(event.target.checked);
+            }}/>
+            <span className="slider">{activeText}</span>
+            <span className="slider">{inactiveText}</span>
+        </label>
+    );
+}
+
+export default ToggleButton;
--- src/client/components/ToggleButton/ToggleButton.story.js
+++ src/client/components/ToggleButton/ToggleButton.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from "@storybook/react"
-import ToggleButton from "./ToggleButton";
+import ToggleButton from "./ToggleButton.component";
 
 storiesOf("ToggleButton", module).add("Toggle button", () => {
-    return <ToggleButton/>
+    return <ToggleButton onChange={(val) => console.log(val)} activeText={'Desktop'} inactiveText={'Mobile'}/>
 });
