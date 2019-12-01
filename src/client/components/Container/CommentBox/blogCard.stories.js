3b1c872dac2af42a58a2598a27431ecd7486b5b6
Girmay <ink423103@yahoo.com>
2019-12-01 23:40:36 +0100

    List changed to Button and props added

--- src/client/components/Container/CommentBox/blogCard.stories.js
+++ src/client/components/Container/CommentBox/blogCard.stories.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import BlogCard from "./blogCard";
+import BlogCard from "./blogCard.Component";
 
 storiesOf("BlogCard", module)
   .add("with text", () => <BlogCard name="blogCardLabel" title="make blog as cards"
-    placeholder="add comment..." type="input"
+    placeholder="add comment..." type="input" titleArray ={['...','pin','mark as done','send to Trolle' ]}
   ></BlogCard>);
 
   export default BlogCard;
--- src/client/components/Container/DropDown/DropDown.Component.jsx
+++ src/client/components/Container/DropDown/DropDown.Component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import List from "../../List/List.Component";
+import Button from "../../Button/Button.Component";
 
 const DropDown = props => {
   return(
-    <div class="dropdown-content">
-      <List title={'...'}/>
-      <List title={'pin'}/>
-      <List title = {'mark as done'} />
-      <List title = {'send to Trolle'} />
+    <div class="dropdown-content" style={dropdownStyle}>
+      {props.titleArray.map(title => <Button title={title} onClickHandle={props.onClickHandle}/> )}   
     </div> 
   )     
 }
-
+const dropdownStyle = {
+  display: 'flex',
+  zIndex: '4',
+  marginTop: '25px'
+}
 export default DropDown;
