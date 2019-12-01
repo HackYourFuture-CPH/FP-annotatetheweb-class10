2964afd4297fb7ac546c97b9bae4ccc286196929
Girmay <ink423103@yahoo.com>
2019-12-01 23:41:53 +0100

    titles added as array

--- src/client/components/Container/DropDown/DropDown.story.js
+++ src/client/components/Container/DropDown/DropDown.story.js
@@ -3,6 +3,6 @@ import { storiesOf } from "@storybook/react";
 import DropDown from "./DropDown.Component";
 
 storiesOf("DropDown", module)
-  .add("with text", () => <DropDown></DropDown>);
+  .add("with text", () => <DropDown titleArray ={['...','pin','mark as done','send to Trolle' ]}></DropDown>);
 
   export default DropDown;
