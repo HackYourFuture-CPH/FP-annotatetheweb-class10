cbbbb2b0a6fa7b4dfba3d45956253d0953694a15
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-12-03 20:14:38 +0100

    fixed navbar test

--- src/client/components/NavBar/NavBar.test.js
+++ src/client/components/NavBar/NavBar.test.js
@@ -4,18 +4,17 @@ import "@testing-library/jest-dom/extend-expect";
 import NavBar from "./NavBar.component";
 
 test("if Nav Bar has correct text",()=>{
-    // const navElements =[
-    //     { title: "Annotate", id: 1 },
-    //     { title: "About", id: 2 },
-    //     { title: "Login", id: 3}
-    //   ];
+    const navElements =[
+        { title: "Annotate", id: 1 },
+        { title: "About", id: 2 },
+        { title: "Login", id: 3}
+      ];
     const href = "#";
     const {container} = render(<NavBar 
-    // navElements ={navElements}
+    navElements ={navElements}
     href ={href}
     />)
-    // expect(navElements).toBe(1);
-    expect(container).toContainObject({ title: "Annotate" })
-})
+    expect(container.children.length).toBe(1);
+});
 
 
