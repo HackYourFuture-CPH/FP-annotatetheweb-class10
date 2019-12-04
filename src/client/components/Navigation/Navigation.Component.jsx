7689a86c0649c426293eb9676ff6884cbeaac77d
abdullahhhhh <abdullah.hussaini@hotmail.com>
2019-12-04 18:31:36 +0100

    Navigation component, css and storybook added

---
+++ src/client/components/Navigation/Navigation.Component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from '@reach/router'
+import './Navigation.css'
+const Navigation = () => {
+    return (
+        
+            <div className='navigation'>
+                <div className="RegisterBtn">
+                    <div className="Rectangle11"></div>
+                    <div className="RegisterText">Register</div>
+                </div>
+                 <div className='Navigate'> 
+                    <div className='Anot'>Annotate</div>
+                    <div className='line4'></div>
+                    <div className='Abt'>About</div>
+                    <div className='lgn'>Login</div>
+                 </div>  
+            </div>
+      
+    )
+}
+
+export default Navigation
---
+++ src/client/components/Navigation/Navigation.story.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Navigation from "./Navigation.Component";
+
+storiesOf("Navigation", module).add("Navigation", () => {
+  return <Navigation ></Navigation>;
+});
