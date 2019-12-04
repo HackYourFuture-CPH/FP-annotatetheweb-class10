00ed1f0a4a25c94e112aeb3aa94e12f2b1809df5
Girmay <ink423103@yahoo.com>
2019-12-04 10:43:09 +0100

    commponent stories import removed

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -1,22 +1,9 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Button } from "@storybook/react/demo";
-import {BlogCard} from "../components/Container/CommentBox/blogCard.stories";
-import {Input} from "../components/Input/Input.story";
-import {DotButton} from "../components/DotButton/DotButton.story";
-import {Anchor} from "../components/Container/DropDown/DropDown.story";
-import {List} from "../components/List/List.story";
-import { from } from "array-flatten";
 
-storiesOf("Button", module)
-  .add("with text", () => <Button>Hello!</Button>)
-  .add("with emoji", () => (
-    <Button>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+import {BlogCard} from "../components/Container/CommentBox/blogCard.stories";
+
+
+
+
   
   
 
