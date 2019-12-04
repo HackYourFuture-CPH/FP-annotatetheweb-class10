a74011be07f5a5aff808c05bbd08f1b275bf153c
Ida <ida.naderian@gmail.com>
2019-12-04 21:57:25 +0100

    create projects and screenshots tables.

---
+++ src/server/seeds/004_00_screenshots.js
@@ -0,0 +1,17 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("screenshots")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("screenshots").insert([
+        {
+          screenshot_id: 1,
+          url: "https://www.google.com/",
+          height: "50",
+          width: "50",
+          fk_project_id: 1
+        }
+      ]);
+    });
+};
