c7ef1fa01209c225b892a8c52bc750b4548a0306
Lilla <korosililla@gmail.com>
2019-12-05 15:35:28 +0100

    added get method for screenshots by id

--- src/server/api/controllers/screenshots.controller.js
+++ src/server/api/controllers/screenshots.controller.js
@@ -1,12 +1,21 @@
-"use strict";
+'use strict';
 
-const knex = require("../../config/db");
-const { paginate } = require("../lib/utils/pagination");
+const knex = require ('../../config/db');
+const {paginate} = require ('../lib/utils/pagination');
 
-const getScreenshots = ({limit, offset, orderBy = "screenshot_id", order} = {}) => {
-  return paginate(knex("screenshots"), {limit, offset, orderBy, order})
-}
+const getScreenshots = (
+  {limit, offset, orderBy = 'screenshot_id', order} = {}
+) => {
+  return paginate (knex ('screenshots'), {limit, offset, orderBy, order});
+};
+
+const getScreenshotById = screenshot_id => {
+  return knex ('screenshots')
+    .where ({ screenshot_id: screenshot_id })
+    .select ('url', 'width', 'height');
+};
 
 module.exports = {
-  getScreenshots
-}
+  getScreenshots,
+  getScreenshotById,
+};
--- src/server/api/routes/screenshots.router.js
+++ src/server/api/routes/screenshots.router.js
@@ -1,21 +1,30 @@
-"use strict";
+'use strict';
 
 // router setup
-const express = require("express");
-const router = express.Router({ mergeParams: true });
+const express = require ('express');
+const router = express.Router ({mergeParams: true});
 
 // controllers
-const screenshotsController = require("../controllers/screenshots.controller");
+const screenshotsController = require ('../controllers/screenshots.controller');
 
 // ENDPOINT: /api/screenshots/ :GET to get all screenshots
-router.get("/", (req, res, next) => {
-  
-  let { limit, offset, orderBy, order } = req.query;
+router.get ('/', (req, res, next) => {
+  // const screenshotId = req.params.id;
+  // console.log(screenshotId);
+  let {limit, offset, orderBy, order} = req.query;
 
   screenshotsController
-    .getScreenshots(limit, offset, orderBy, order)
-    .then(result => res.json(result))
-    .catch(next);
+    .getScreenshots (limit, offset, orderBy, order)
+    .then (result => res.json (result))
+    .catch (next);
+});
+
+// Get screenshot by screenshot_id
+router.get ('/:screenshot_id', (req, res, next) => {
+  screenshotsController
+    .getScreenshotById (req.params.screenshot_id)
+    .then (result => res.json (result))
+    .catch (next);
 });
 
 module.exports = router;
