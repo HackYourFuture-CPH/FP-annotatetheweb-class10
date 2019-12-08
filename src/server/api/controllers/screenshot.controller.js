c19ed7b5f9a8e6aff88ec958cde0bd816908d250
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-12-08 13:05:36 +0100

    remove the get, delete, patch request

--- src/server/api/controllers/screenshot.controller.js
+++ src/server/api/controllers/screenshot.controller.js
@@ -1,41 +1,6 @@
 "use strict";
 
 const knex = require("../../config/db");
-const { paginate } = require("../lib/utils/pagination");
-
-const getScreenshots = ({
-  limit,
-  offset,
-  orderBy = "screenshot_id",
-  order
-} = {}) => {
-  return paginate(knex("screenshots"), { limit, offset, orderBy, order });
-};
-
-const getScreenshotById = async screenshotId => {
-  return knex("screenshots").where({ screenshot_id: screenshotId });
-};
-
-const editScreenshot = async (screenshotId, updatedScreenshot) => {
-  return knex("screenshots")
-    .where({ screenshot_id: screenshotId })
-    .update({
-      url: updatedScreenshot.url,
-      key: updatedScreenshot.key,
-      height: updatedScreenshot.height,
-      width: updatedScreenshot.width,
-      created_at: updatedScreenshot.created_at,
-      updated_at: updatedScreenshot.updated_at,
-      deleted_at: updatedScreenshot.deleted_at,
-      fk_project_id: updatedScreenshot.fk_project_id
-    });
-};
-
-const deleteScreenshot = async (screenshotId, req) => {
-  return knex("screenshots")
-    .where({ screenshot_id: screenshotId })
-    .del();
-};
 
 const createScreenshot = async body => {
   const [screenshotId] = await knex("screenshots").insert({
@@ -55,9 +20,5 @@ const createScreenshot = async body => {
 };
 
 module.exports = {
-  getScreenshots,
-  getScreenshotById,
-  deleteScreenshot,
-  createScreenshot,
-  editScreenshot
+  createScreenshot
 };
