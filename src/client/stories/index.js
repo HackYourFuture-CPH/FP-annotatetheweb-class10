3447eb36c9eecd5b07c95388af52f3c480c3ca3f
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-12-08 14:20:58 +0100

    changes

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "@storybook/react/demo";
-<<<<<<< HEAD
 import {FormTitle} from '../components/FormTitle/FormTitle.story';
 import {FormButtons} from '../components/Button/Button.story';
 import {FormField} from '../components/FormField/FormField.story';
 import {FormLine} from '../components/FormLine/FormLine.story';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-=======
 import { Content } from "../components/content/Content.story";
 import { SidebarMenu } from '../components/SidebarMenu/SidebarMenu.story';
->>>>>>> da5ad3bdae26d6b96dc11cabef36eefb31dfa41c
