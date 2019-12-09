870d075da77fff946637191beafc9cba4906bbd7
Girmay <ink423103@yahoo.com>
2019-12-09 21:26:48 +0100

    bind is removed

--- src/client/components/Container/DropDown/DropDown.Component.jsx
+++ src/client/components/Container/DropDown/DropDown.Component.jsx
@@ -5,7 +5,7 @@ const DropDown = props => {
   const id = props.titleArray;
   return(
     <div class="dropdown-content" style={dropdownStyle}>
-      { props.titleArray.map(element => <Button title={element.title} onClickHandle={props.onClickHandle.bind(this,element.id)}/> )}   
+      { props.titleArray.map(element => <Button title={element.title} onClickHandle={props.onClickHandle}/> )}   
     </div> 
   )     
 }
