b20afddfaf271552dc447f84226d870c98b095a9
Girmay <ink423103@yahoo.com>
2019-12-09 21:26:03 +0100

    props added

--- src/client/components/Button/Button.storybook.js
+++ src/client/components/Button/Button.storybook.js
@@ -3,5 +3,5 @@ import { storiesOf } from "@storybook/react";
 import Button from "./Button.Component";
 
 storiesOf("Button", module)
-  .add("with text", () => <Button title={"Button"} ></Button>);
+  .add("with text", () => <Button title={props.title} ></Button>);
   export default Button;
