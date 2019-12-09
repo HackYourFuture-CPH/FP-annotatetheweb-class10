bcb0c22798460241e800def6b968ffb24a709de3
Girmay <ink423103@yahoo.com>
2019-12-09 21:29:17 +0100

    props added as object

--- src/client/components/Container/DropDown/DropDown.story.js
+++ src/client/components/Container/DropDown/DropDown.story.js
@@ -3,6 +3,6 @@ import { storiesOf } from "@storybook/react";
 import DropDown from "./DropDown.Component";
 
 storiesOf("DropDown", module)
-  .add("with text", () => <DropDown titleArray ={['...','pin','mark as done','send to Trolle' ]}></DropDown>);
+  .add("with text", () => <DropDown titleArray ={[{id:1,title:'...'},{id:2,title:'pin'},{id:3,title:'mark as done'},{id:4, title:'send to Trolle'}]}></DropDown>);
 
   export default DropDown;
