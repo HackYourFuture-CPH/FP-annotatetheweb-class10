df7a46767530fbe2ce3da1b810084daeff07e15a
Girmay <ink423103@yahoo.com>
2019-12-09 20:30:09 +0100

    annotations controller table input data added"

--- src/server/api/controllers/annotations.controller.js
+++ src/server/api/controllers/annotations.controller.js
@@ -4,11 +4,15 @@ const knex = require("../../config/db");
 
 
 const createAnnotation = async body => {
-  const [annotationId] = await knex("annotations").insert({   
+  const [annotationId] = await knex("annotations").insert({
+    annotation_id: 1,   
     title: body.title,
     area: body.area,
     description: body.description,
-    fk_screenshot_id: body.fk_screenshot_id   
+    fk_screenshot_id: body.fk_screenshot_id,
+    created_at: new Date(),
+    updated_at: null,
+    deleted_at: null
   });
 
   return {
