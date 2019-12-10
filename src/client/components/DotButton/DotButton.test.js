5fa246d85bcd706ede94e5608fbe16d8e339ddb6
Girmay <ink423103@yahoo.com>
2019-12-10 10:52:49 +0100

    resolved component name case sensitive

--- src/client/components/DotButton/DotButton.test.js
+++ src/client/components/DotButton/DotButton.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import DotButton from "./DotButton.component";
+import DotButton from "./DotButton.Component";
 
 test("if button has correct text", () => {
   const title = "...";
