0a093e2f9c393ecdd28d0b8381b3b7ed42cf571a
Girmay <ink423103@yahoo.com>
2019-12-14 17:58:26 +0100

    JSON stringify method used

--- src/server/api/controllers/annotations.controller.js
+++ src/server/api/controllers/annotations.controller.js
@@ -4,19 +4,19 @@ const knex = require("../../config/db");
 
 
 const createAnnotation = async body => {
-  console.log(body.created_at)
-  const [annotationId] = await knex("annotations").insert({   
-    title: body.title,
-    area: body.area,
-    description: body.description,
-    fk_screenshot_id: body.fk_screenshot_id    
-  });
+    console.log(body.created_at)
+    const [annotationId] = await knex("annotations").insert({
+        title: body.title,
+        area: JSON.stringify(body.area),
+        description: body.description,
+        fk_screenshot_id: body.fk_screenshot_id
+    });
 
-  return {
-    successful: true
-  };
+    return {
+        successful: true
+    };
 };
 
 module.exports = {
-  createAnnotation
+    createAnnotation
 };
