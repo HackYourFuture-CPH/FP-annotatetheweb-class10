e2f3b066037502b61e42177840ddfe8da3eb2c01
abdullahhhhh <abdullah.hussaini@hotmail.com>
2019-12-14 23:49:06 +0100

    footer opdated

--- src/client/components/Footer/Footer.component.jsx
+++ src/client/components/Footer/Footer.component.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer-btn">
-      <div className='Rectangle-btn'></div>
+      <div className='rectangle-btn'></div>
       <div className="content-btn">
         Created by: <a href="https://www.hackyourfuture.dk" target="_blank">  HACK YOUR FUTURE
         </a> <br/>  
