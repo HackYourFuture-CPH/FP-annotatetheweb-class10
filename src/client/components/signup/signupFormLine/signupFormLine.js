54896efd4f8cc908603adf328772f217e04d1830
Mohseni <mohseni.mr93@gmail.com>
2019-12-14 18:54:08 +0100

    removed files

--- src/client/components/signup/signupFormLine/signupFormLine.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../signup.css";
-
-const SignupFormLine = () => {
-  return <div className="signupFormLine">Or</div>;
-};
-export default SignupFormLine;
--- src/client/components/signup/title/signupTitle.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../signup.css";
-
-const SignupTitle = () => {
-  return <div className="signupTitle">Sign up to Annotate the web</div>;
-};
-
-export default SignupTitle;
