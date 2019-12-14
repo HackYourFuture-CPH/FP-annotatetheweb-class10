f99aafdbdb27d7835a9f98d146915ac639dc46d6
Girmay <ink423103@yahoo.com>
2019-12-14 13:23:44 +0100

    key added to list

--- src/client/container/DropDown/DropDown.component.jsx
+++ src/client/container/DropDown/DropDown.component.jsx
@@ -3,10 +3,10 @@ import Button from "../../components/Button/Button.component";
 import dropdownStyle from "./Them";
 
 const DropDown = props => {
-  const id = props.titleArray;
+  
   return(
     <div class="dropdown-content" style={dropdownStyle}>
-      { props.titleArray.map(element => <Button title={element.title} onClickHandle={props.onClickHandle}/> )}   
+      { props.titleArray.map(element => <Button key={element.id} title={element.title} onClickHandle={props.onClickHandle}/> )}   
     </div> 
   )     
 }
