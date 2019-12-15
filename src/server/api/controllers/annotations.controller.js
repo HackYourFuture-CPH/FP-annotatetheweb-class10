b3c160d810625ae45a22a3c8c2ce0ffc78bd09bd
Mohseni <mohseni.mr93@gmail.com>
2019-12-15 16:09:10 +0100

    annotation fixed

--- src/server/api/controllers/annotations.controller.js
+++ src/server/api/controllers/annotations.controller.js
@@ -1,25 +1,48 @@
 "use strict";
 
-const express = require("express");
-const router = express.Router();
+const knex = require("../../config/db");
+const error = require("../lib/utils/http-error");
 
-// Router imports
-const modulesRouter = require("./modules.router");
-const annotationsRouter = require("./annotations.router");
-const userRouter = require("./users.router");
-const screenshotsRouter = require("./screenshots.router");
+const getAnnotations = async req => {
+  try {
+    return await knex("annotations").select(
+      "annotations.annotation_id",
+      "annotations.title",
+      "annotations.description",
+      "annotations.fk_screenshot_id",
+      "annotations.area",
+      "annotations.updated_at",
+      "annotations.created_at",
+      "annotations.deleted_at"
+    );
+  } catch (error) {
+    return error.message;
+  }
+};
+const getAnnotationsById = async id => {
+  try {
+    const annotations = await knex("annotations")
+      .select(
+        "annotations.id as id",
+        "title",
+        "description",
+        "fk_screenshot_id",
+        "area",
+        "updated_at",
+        "created_at",
+        "deleted_at"
+      )
+      .where({ id: id });
+    if (annotation.length === 0) {
+      throw new error(`encorrect entry with the id of ${id}`, 404);
+    }
+    return annotations;
+  } catch (error) {
+    return error.message;
+  }
+};
 
-// swagger-ui-express
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../../config/swagger.json");
-
-// Route for Swagger API Documentation
-router.use(`/documentation`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Application routes
-router.use(`/modules`, modulesRouter);
-router.use(`/users`, userRouter);
-router.use(`/annotations`,annotationsRouter);
-router.use(`/screenshots`, screenshotsRouter);
-
-module.exports = router;
+module.exports = {
+  getAnnotations,
+  getAnnotationsById
+};
