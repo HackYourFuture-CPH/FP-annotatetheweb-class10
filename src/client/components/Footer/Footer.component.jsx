4b1d2674e352db294894007f239963b7d7112c12
abdullahhhhh <abdullah.hussaini@hotmail.com>
2019-12-15 12:31:10 +0100

    footer opdated

--- src/client/components/Footer/Footer.component.jsx
+++ src/client/components/Footer/Footer.component.jsx
@@ -4,11 +4,25 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer-btn">
-      <div className='rectangle-btn'></div>
+      <div className="rectangle-btn"></div>
       <div className="content-btn">
-        Created by: <a href="https://www.hackyourfuture.dk" target="_blank">  HACK YOUR FUTURE
-        </a> <br/>  
-         and  <a href="https://www.codeart.dk" target="_blank">Code: art 
+        Created by:{" "}
+        <a
+          href="https://www.hackyourfuture.dk"
+          target="_blank"
+          className="content-logos"
+        >
+          {" "}
+          HACK YOUR FUTURE
+        </a>{" "}
+        <br />
+        and{" "}
+        <a
+          href="https://www.codeart.dk"
+          target="_blank"
+          className="content-logos"
+        >
+          Code: art
         </a>
       </div>
     </footer>
