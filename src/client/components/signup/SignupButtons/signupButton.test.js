0c47f7d60406e74c3159a2e1aba8b3b1aeaeb89b
Mohseni <mohseni.mr93@gmail.com>
2019-12-15 14:49:13 +0100

    done

--- src/client/components/signup/SignupButtons/signupButton.test.js
+++ src/client/components/signup/SignupButtons/signupButton.test.js
@@ -3,11 +3,12 @@ import { render, fireEvent } from "@testing-library/react";
 import SignupButotns from "./SignupButtons";
 import SignupButtons from "./SignupButtons";
 it("click on button- function onClick is called", () => {
-  let title;
+  let title = [
+    {title : "create Account"},
+    {title: "signup with Google"}
+  ];
   const handleClick = jest.fn();
-  if (title === "Create Account" || title === "signup with Google") {
-    const { getByText } = render(<SignupButtons onClick={handleClick} />);
+    const { getByText } = render(<SignupButtons onClick={handleClick} title= {title} />);
     fireEvent.click(getByText(title));
     expect(handleClick).toHaveTextContent("1");
-  }
 });
