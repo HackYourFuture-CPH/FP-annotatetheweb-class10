4ac651cadaf9427f598a163f5e7b65571ab8c798
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-12-16 22:43:06 +0100

    get key from CodeArt api and add to the database

--- src/server/api/controllers/screenshots.controller.js
+++ src/server/api/controllers/screenshots.controller.js
@@ -1,35 +1,60 @@
-'use strict';
+"use strict";
 
-const knex = require ('../../config/db');
-const {paginate} = require ('../lib/utils/pagination');
+const fetch = require("cross-fetch");
+const knex = require("../../config/db");
+const { paginate } = require("../lib/utils/pagination");
 
-const getScreenshots = (
-  {limit, offset, orderBy = 'screenshot_id', order} = {}
-) => {
-  return paginate (knex ('screenshots'), {limit, offset, orderBy, order});
+const getScreenshots = ({
+  limit,
+  offset,
+  orderBy = "screenshot_id",
+  order
+} = {}) => {
+  return paginate(knex("screenshots"), { limit, offset, orderBy, order });
 };
 
 const getScreenshotByKey = key => {
-  return knex ('screenshots')
-    .where ({ key: key })
-    .select ('url', 'width', 'height');
+  return knex("screenshots")
+    .where({ key: key })
+    .select("url", "width", "height");
 };
 
 const createScreenshot = async body => {
-  const [screenshotId] = await knex("screenshots").insert({
-    url: body.url,
-    key: body.key,
-    height: body.height,
-    width: body.width,
-    created_at: body.created_at,
-    updated_at: body.updated_at,
-    deleted_at: body.deleted_at,
-    fk_project_id: body.fk_project_id
-  });
+  const urlToScreenshot = "https://google.com";
 
-  return {
-    successful: true
-  };
+  await fetch(
+    `https://api.annotatetheweb.com/screenshot/1.0/RequestScreenshot?url=${urlToScreenshot}`,
+    {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({})
+    }
+  )
+    .then(async function(res) {
+      const keyValue = await res.json();
+      return keyValue;
+    })
+
+    .then(async function(keyValue) {
+      const [screenshotId] = await knex("screenshots").insert({
+        url: body.url,
+        key: keyValue.key,
+        height: body.height,
+        width: body.width,
+        created_at: body.created_at,
+        updated_at: body.updated_at,
+        deleted_at: body.deleted_at,
+        fk_project_id: body.fk_project_id
+      });
+
+      return {
+        successful: true
+      };
+    });
 };
 
 module.exports = {
