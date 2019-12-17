a88fb3df2a378bf416722ac7386ae9d66e7e30bf
Zaki Wasik <zakiwa@gmail.com>
2019-12-17 23:23:37 +0100

    Tailor eslint config to project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,23 @@ module.exports = {
   // Add custom rules here
   rules: {
     'arrow-parens': 0,
-    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
     'no-unexpected-multiline': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 1,
+    'react/prop-types': 1,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/camelcase': 1,
+    'func-names': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'no-restricted-globals': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    'no-use-before-define': 0,
+    'consistent-return': 0,
+    'jsx-a11y/label-has-associated-control': 0,
   },
 };
