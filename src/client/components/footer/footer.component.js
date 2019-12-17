a35f1a72d4d9327f54c92509444e1e2d5b371a8d
abdullahhhhh <abdullah.hussaini@hotmail.com>
2019-12-17 14:14:39 +0100

    footer component

---
+++ src/client/components/footer/footer.component.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./footer.css";
+import hyf_logo_black_background from "../../assets/images/hyf_logo_black_background.png";
+import codeart_logo_black_background from "../../assets/images/codeart_logo_black_background.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer-btn">
+      <div className="rectangle-btn"></div>
+      <a href="https://www.hackyourfuture.dk" target="_blank">
+        <img className="footer-hyf-logo" src={hyf_logo_black_background} />
+      </a>
+      <a href="https://www.codeart.dk" target="_blank">
+        <img
+          className="footer-codeart-logo"
+          src={codeart_logo_black_background}
+        />
+      </a>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/client/components/footer/footer.story.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Footer from "./footer.component";
+storiesOf("Footer", module).add("Footer", () => {
+  return <Footer />;
+});
