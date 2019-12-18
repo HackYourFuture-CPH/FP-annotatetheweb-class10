61558cc38a01e75fb942f59c399698731a3d25ca
abdullahhhhh <abdullah.hussaini@hotmail.com>
2019-12-18 22:10:59 +0100

    footer is updated

--- src/client/components/footer/footer.component.js
+++ src/client/components/footer/footer.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 import hyf_logo_black_background from "../../assets/images/hyf_logo_black_background.png";
 import codeart_logo_black_background from "../../assets/images/codeart_logo_black_background.png";
 
@@ -9,11 +9,9 @@ const Footer = () => {
       <div className="rectangle-btn"></div>
       <a href="https://www.hackyourfuture.dk" target="_blank">
         <img className="footer-hyf-logo" src={hyf_logo_black_background} />
-      </a>
+      </a> 
       <a href="https://www.codeart.dk" target="_blank">
-        <img
-          className="footer-codeart-logo"
-          src={codeart_logo_black_background}
+        <img className="footer-codeart-logo" src={codeart_logo_black_background}
         />
       </a>
     </footer>
--- src/client/components/footer/footer.story.js
+++ src/client/components/footer/footer.story.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import Footer from "./footer.component";
+import {storiesOf} from "@storybook/react";
+import Footer from "./Footer.component";
 storiesOf("Footer", module).add("Footer", () => {
   return <Footer />;
 });
