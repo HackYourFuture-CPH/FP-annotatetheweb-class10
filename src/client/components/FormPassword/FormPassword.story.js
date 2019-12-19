e7e50ca05b6c8b4918f2876f22ad4ccbb098c670
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-12-19 13:35:48 +0100

    unnecessary files deleted

--- src/client/components/FormPassword/FormPassword.story.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import FormPassword from './FormPassword.component';
-
-storiesOf('FormPassword',module).add('with password' ,() => <FormPassword/>)
