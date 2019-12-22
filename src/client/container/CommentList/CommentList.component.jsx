53b01c7256cbde78247c24d455cf7b4429098d6a
Girmay <ink423103@yahoo.com>
2019-12-22 10:39:13 +0100

    Id generating function deleted

--- src/client/container/CommentList/CommentList.component.jsx
+++ src/client/container/CommentList/CommentList.component.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import List from "../../components/List/List.component";
 
 const CommentList = props => {
-    
+
     return(
         <div className="comment-list">
-            {props.inputValue.map(item =>            
-                <List key={item.id} title={item.comment}/>    
+            {props.inputValue.map(item =>
+                <List title={item.comment}/>    
             )}
         </div>
-    )     
+    )
 }
 
 export default CommentList;
