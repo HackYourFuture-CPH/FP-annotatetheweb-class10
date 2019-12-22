31e2a9844717e403fc26aca3da0ec3c087c088d1
rakhshanda-rashid <rakhshandarashid98@gmail.com>
2019-12-22 13:16:18 +0100

    .git file deleted

--- src/client/components/FormSignIn/FormSignIn.story.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import FormSignIn from './FormSignIn.component';
-
-storiesOf('FormSignIn',module)
-.add('login' ,() => <FormSignIn />)
