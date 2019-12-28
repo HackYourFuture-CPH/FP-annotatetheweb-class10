e8a9987241d231c24b3983903b11bdf409bf5be8
Laila1222 <korosililla@gmail.com>
2019-12-28 23:02:34 +0100

    added post and put endpoints

--- src/server/api/controllers/projects.controller.js
+++ src/server/api/controllers/projects.controller.js
@@ -1,39 +1,51 @@
 'use strict';
 
-const knex = require ('../../config/db');
-const {paginate} = require ('../lib/utils/pagination');
+const knex = require('../../config/db');
+const { paginate } = require('../lib/utils/pagination');
 
 // Return all projects
-const getProjects = ({limit, offset, orderBy = 'project_id', order} = {}) => {
-  return paginate (knex ('projects'), {limit, offset, orderBy, order});
+const getProjects = ({ limit, offset, orderBy = 'project_id', order } = {}) => {
+  return paginate(knex('projects'), { limit, offset, orderBy, order });
 };
 
 // Return project by project_id
 const getProjectById = (project_id) => {
-  return knex ('projects')
-    .where ({project_id: project_id})
-    .select ('*');
+  return knex('projects')
+    .where({ project_id: project_id })
+    .select('*');
 };
 
 // Return projects for a specific userId
 const getProjectByUserId = (fk_user_id) => {
-    return knex ('projects')
-    .where({ fk_user_id: fk_user_id})
-    .select ('*');
-}
+  return knex('projects')
+    .where({ fk_user_id: fk_user_id })
+    .select('*');
+};
 
 const updateProject = (project_id, body) => {
-    return knex('projects')
-    .where({ project_id: project_id})
+    console.log(body.name);
+  return knex('projects')
+    .where({ project_id: project_id })
     .update({
-        name: body.name,
-        updated_at: new Date()
+      name: body.name,
+      updated_at: new Date(),
     });
-}
+};
+
+const createProject = (body) => {
+  console.log(body);
+  return knex('projects').insert({
+    name: body.name,
+    fk_user_id: body.fk_user_id,
+    created_at: new Date(),
+  });
+
+};
 
 module.exports = {
   getProjects,
   getProjectById,
   getProjectByUserId,
-  updateProject
+  updateProject,
+  createProject,
 };
--- src/server/api/routes/projects.router.js
+++ src/server/api/routes/projects.router.js
@@ -1,45 +1,53 @@
 'use strict';
 
 // router setup
-const express = require ('express');
-const router = express.Router ({mergeParams: true});
+const express = require('express');
+const router = express.Router({ mergeParams: true });
 
 // controllers
-const projectsController = require ('../controllers/projects.controller');
+const projectsController = require('../controllers/projects.controller');
 
 // ENDPOINT: /api/projects/ :GET to get all projects
-router.get ('/', (req, res, next) => {
-  let {limit, offset, orderBy, order} = req.query;
+router.get('/', (req, res, next) => {
+  let { limit, offset, orderBy, order } = req.query;
 
   projectsController
-    .getProjects (limit, offset, orderBy, order)
-    .then (result => res.json (result))
-    .catch (next);
+    .getProjects(limit, offset, orderBy, order)
+    .then((result) => res.json(result))
+    .catch(next);
 });
 
 // ENDPOINT: /api/projects/:id get user by id
-router.get ('/:project_id', (req, res, next) => {
+router.get('/:project_id', (req, res, next) => {
   console.log(req.params);
   projectsController
-    .getProjectById (req.params.project_id)
-    .then (result => res.json (result))
-    .catch (next);
+    .getProjectById(req.params.project_id)
+    .then((result) => res.json(result))
+    .catch(next);
 });
 
 // ENDPOINT: /api/projects/user/:fk_user_id
 router.get('/user/:fk_user_id', (req, res, next) => {
-    projectsController
-    .getProjectByUserId (req.params.fk_user_id)
-    .then(result => res.json(result))
-    .catch (next);
-})
+  projectsController
+    .getProjectByUserId(req.params.fk_user_id)
+    .then((result) => res.json(result))
+    .catch(next);
+});
 
 // ENDPOINT: /api/projects/:project_id :PUT - update project
 router.put('/:project_id', (req, res, next) => {
-    projectsController
+  projectsController
     .updateProject(req.params.project_id, req.body)
+    .then((result) =>res.json({ success: result === 1 }))
+    .catch(next);
+});
+
+// ENDPOINT: api/projects/ :POST - create new project
+router.post('/', (req, res, next) => {
+  projectsController
+    .createProject(req.body)
     .then((result) => res.json({ success: result === 1 }))
     .catch(next);
-})
+});
 
 module.exports = router;
