086239540e83682789be5dd2a48a6a1541b93606
Laila1222 <korosililla@gmail.com>
2020-01-07 16:15:40 +0100

    copied files to new folder with proper name

---
+++ src/client/components/FormLoginRegister/FormLoginRegister.story.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import FormLoginRegister from './FormLoginRegister.component';
+
+storiesOf('FormLoginRegister', module).add('register', () => (
+  <FormLoginRegister
+    formLoginRegister="form-login-register"
+    text="Do not have account yet?"
+    register="Register"
+    classRegister="register-class"
+    linewrapper="line-wrapper"
+  />
+));
