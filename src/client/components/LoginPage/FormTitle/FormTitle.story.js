84f869469e878c0a4f70a7073f2cda8a51c5c1d6
Laila1222 <korosililla@gmail.com>
2020-01-08 13:41:52 +0100

    modified form-title

--- src/client/components/LoginPage/FormTitle/FormTitle.story.js
+++ src/client/components/LoginPage/FormTitle/FormTitle.story.js
@@ -2,4 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import FormTitle from './FormTitle.component';
 
-storiesOf('FormTitle', module).add('with title', () => <FormTitle />);
+storiesOf('FormTitle', module).add('with title', () => <FormTitle/>);
