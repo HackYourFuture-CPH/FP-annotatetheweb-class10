b286f85bfad842312c50461019f7b95955d098bd
Girmay <ink423103@yahoo.com>
2020-01-08 23:28:31 +0100

    ProjectPage storybook added

---
+++ src/client/components/Pages/Projects/ProjectPage.story.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ProjectPage from './ProjectPage.component';
+
+storiesOf('ProjectPage ', module).add('project', () => <ProjectPage />);
