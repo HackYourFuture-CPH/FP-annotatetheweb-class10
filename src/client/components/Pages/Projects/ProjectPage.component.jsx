2b6272afc295ec57c6d62894f2600214145026d4
Girmay <ink423103@yahoo.com>
2020-01-10 19:12:48 +0100

    ProjectPage component added

--- src/client/components/Pages/Projects/ProjectPage.component.jsx
+++ src/client/components/Pages/Projects/ProjectPage.component.jsx
@@ -4,14 +4,47 @@ import ProfileSummery from '../../ProfileSummary/ProfileSummary.component';
 import Header from '../../Header/Header.Component';
 import RegisterButton from '../../RegisterButton/RegisterButton.component';
 import Button from '../../Button/Button.component';
+import Footer from '../../Footer/Footer.component';
+import samplePhoto from '../../../assets/images/profile.png';
+import BlogCard from '../../../container/CommentBox/BlogCard.component';
+import './ProjectPage.css';
+
+const profile = {
+  src: samplePhoto,
+  alt: 'profile image',
+};
+const headerTitle = "annotate the web";
 
 const ProjectPage = () => {
-  return (<div>
-    <SidebarMenu />
-    <Header />
-    <Button title="Login"/>
-    <RegisterButton />
-    <ProfileSummery />
+  return (<div className="project-page-container">
+    <div>
+      <SidebarMenu />
+      <div className="project-page-rightside-container">
+        <div>
+          <Header title ={headerTitle}/>
+        </div>
+        
+        <div >
+          <Button className="project-page-button" title="Login"/>
+        </div>
+        
+        <div >
+          <RegisterButton title="Register"/>
+        </div>
+        
+        <div className="profile-container">
+          <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+        </div>
+        <div>
+          <BlogCard />
+        </div>
+    </div>
+      </div>
+      
+    <div className="project-page-footer">
+      <Footer /> 
+    </div>
+      
 
   </div>);
 };
