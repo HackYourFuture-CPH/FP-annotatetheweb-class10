857398cedbbcb5bd44a32fe0757f0fa0ed027a2a
Girmay <ink423103@yahoo.com>
2020-01-10 19:14:01 +0100

    ProjectPage story added

--- src/client/components/Pages/Projects/ProjectPage.story.js
+++ src/client/components/Pages/Projects/ProjectPage.story.js
@@ -2,4 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ProjectPage from './ProjectPage.component';
 
+
 storiesOf('ProjectPage ', module).add('project', () => <ProjectPage />);
+
+
+
