30110af2d2f5943976d552fab97e796f299ac8d6
Zaki Wasik <zakiwa@gmail.com>
2020-01-12 15:44:15 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const webpack = require("webpack");
-var dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
 const outputDirectory = "dist";
 
