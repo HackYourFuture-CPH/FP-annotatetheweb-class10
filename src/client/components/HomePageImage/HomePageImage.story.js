dd594cf63e7eb460adb62fe76738849014007cb2
rakhshanda-rashid <49167176+rakhshanda-rashid@users.noreply.github.com>
2020-01-13 01:40:37 +0100

    Delete HomePageImage.story.js

--- src/client/components/HomePageImage/HomePageImage.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import imageHomePage from '../../assets/images/imageHomePage.jpg';
-import HomePageImage from './HomePageImage.component';
-
-storiesOf('HomePageImage', module).add('HomePageImage', () => {
-  return <HomePageImage classImage="imageHome" homeImage={imageHomePage} />;
-});
