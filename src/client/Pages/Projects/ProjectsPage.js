3eb09b64b4bbace0cb58b3353b71188d12ec1f7f
Olha Afanasieva <afanasievaolja@gmail.com>
2020-01-14 23:15:24 +0100

    delete folder Pages

--- src/client/Pages/Projects/ProjectsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProjectsPage = () => {
-  return <div />;
-};
-
-export default ProjectsPage;
--- src/client/Pages/Screenshots/ScreenshotsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ScreenshotsPage = () => {
-  return <div />;
-};
-
-export default ScreenshotsPage;
