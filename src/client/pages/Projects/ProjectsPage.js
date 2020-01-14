76e97d0f380523d205e3de581000a2495524dbb9
Olha Afanasieva <afanasievaolja@gmail.com>
2020-01-14 23:16:26 +0100

    create folder pages

---
+++ src/client/pages/Projects/ProjectsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+  return <div />;
+};
+
+export default ProjectsPage;
---
+++ src/client/pages/Screenshots/ScreenshotsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ScreenshotsPage = () => {
+  return <div />;
+};
+
+export default ScreenshotsPage;
