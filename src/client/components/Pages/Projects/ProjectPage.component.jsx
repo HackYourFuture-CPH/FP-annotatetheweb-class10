051357a33b239a45a539f6accec5746a00f7d24c
Girmay <ink423103@yahoo.com>
2020-01-17 22:38:23 +0100

    component changed to smart inorder to use functions of class properties

--- src/client/components/Pages/Projects/ProjectPage.component.jsx
+++ src/client/components/Pages/Projects/ProjectPage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SidebarMenu from '../../SidebarMenu/SidebarMenu.component';
 import ProfileSummery from '../../ProfileSummary/ProfileSummary.component';
 import Header from '../../Header/Header.Component';
@@ -8,72 +8,81 @@ import Footer from '../../Footer/Footer.component';
 import samplePhoto from '../../../assets/images/profile.png';
 import BlogCard from '../../../container/CommentBox/BlogCard.component';
 import imageLogin from '../../../assets/images/imageLogin.jpg';
-
 import './ProjectPage.css';
 
-const profile = {
-  src: samplePhoto,
-  alt: 'profile image',
-};
-const headerTitle = "annotate the web";
-function myFunction() {
-  this.classList.toggle("change");
-}
 
-const ProjectPage = () => {
-  return (<div className="project-page-container">
-    <div>
-      <SidebarMenu /> 
-      <div className="mobilemenu-menuwrap">
-        <div className="header-mobile">
-          <Header title ={headerTitle}/>
-        </div>
-        <div className="container" onclick={myFunction}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
-        </div>
-      </div>  
+class ProjectPage extends Component {
+
+  
+   myFunction=(x)=> {
+    x.classList.toggle("change");
+  }
+  onclickHandler=()=>{
+   
+  }
+  
+  render(){
+    const profile = {
+      src: samplePhoto,
+      alt: 'profile image',
+    };
+    const headerTitle = "annotate the web";
+    return (
+    <div className="project-page-container">
       <div>
-          <img alt="login" src={imageLogin} className="login-page-image" />
-        </div>   
-      <div className="project-page-rightside-container">
-        <div className="project-page-rightside-innerContainer">
-          <div>
+        <SidebarMenu /> 
+        <div className="mobilemenu-menuwrap">
+          <div className="header-mobile">
             <Header title ={headerTitle}/>
           </div>
-          
-          <div >
-            <Button buttonClassName="project-page-button" title="Login"/>
+          <div className="container" onclick={this.onclickHandler}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
           </div>
-          
-          <div >
-            <RegisterButton title="Register" />
+        </div>  
+        <div>
+            <img alt="login" src={imageLogin} className="login-page-image" />
+          </div>   
+        <div className="project-page-rightside-container">
+          <div className="project-page-rightside-innerContainer">
+            <div>
+              <Header title ={headerTitle}/>
+            </div>
+            
+            <div >
+              <Button buttonClassName="project-page-button" title="Login"/>
+            </div>
+            
+            <div >
+              <RegisterButton title="Register" />
+            </div>
+            
+            <div className="profile-container">
+              <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+            </div>
           </div>
-          
-          <div className="profile-container">
-            <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+          <div className="comment-container">
+            <div>
+              <BlogCard />
+            </div>
+            <div>
+              <BlogCard />
+            </div>
+            <div>
+              <BlogCard />
+            </div>
           </div>
+            
         </div>
-        <div className="comment-container">
-          <div>
-            <BlogCard />
-          </div>
-          <div>
-            <BlogCard />
-          </div>
-          <div>
-            <BlogCard />
-          </div>
-        </div>
-          
-      </div>
-      
-    </div>      
-    <div className="project-page-footer">
-      <Footer /> 
-    </div>     
-  </div>);
+        
+      </div>      
+      <div className="project-page-footer">
+        <Footer /> 
+      </div>     
+    </div>);
+  }
+  
 };
 
 export default ProjectPage;
