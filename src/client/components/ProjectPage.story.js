34a2d4bfe91052011e28b6aa418b299909b1abf6
Girmay <ink423103@yahoo.com>
2020-01-20 16:42:25 +0100

    file name modified

---
+++ src/client/components/ProjectPage.story.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ProjectPage from '../pages/Projects/ProjectsPage';
+
+
+storiesOf('ProjectPage ', module).add('project', () => <ProjectPage />);
