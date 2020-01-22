1d43cfda76e424fd84eb6a9c5b8c7b3b7e1b1a7f
bytewiz <niels@bytewiz.io>
2020-01-22 21:53:43 +0100

    Remove commented code

--- src/client/pages/Login/LoginPage.js
+++ src/client/pages/Login/LoginPage.js
@@ -23,27 +23,7 @@ class LoginPage extends Component {
       password: '',
       isLoading: false,
     };
-    // this.authListener = this.authListener.bind(this);
   }
-
-  // componentDidMount() {
-  //   this.authListener();
-  // }
-
-  // authListener() {
-  //   auth.onAuthStateChanged((user) => {
-  //     this.setState({ isLoading: true });
-  //     console.log(user);
-  //     if (user) {
-  //       this.setState({ user });
-  //       localStorage.setItem('user', user.uid);
-  //     } else {
-  //       this.setState({ user: null });
-  //       localStorage.removeItem('user');
-  //     }
-  //     this.setState({ isLoading: false });
-  //   });
-  // }
 
   handleChange = (e) => {
     this.setState({ [e.target.type]: e.target.value });
