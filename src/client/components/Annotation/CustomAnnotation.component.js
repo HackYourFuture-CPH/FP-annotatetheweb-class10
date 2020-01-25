ee784b0ced0998537446bb220ecbfa110cc68ed0
Girmay <ink423103@yahoo.com>
2020-01-25 15:07:26 +0100

    CustomAnnotation functions moved to the the parent component where states are defined

--- src/client/components/Annotation/CustomAnnotation.component.js
+++ src/client/components/Annotation/CustomAnnotation.component.js
@@ -3,54 +3,17 @@ import Annotation from 'react-image-annotation';
 import EditorWrapper from './EditorWrapper';
 
 class CustomAnnotation extends Component {
-  state = {
-    annotations: [],
-    annotation: {},
-    data: {},
-  };
-
-  onChange = (annotation, value = {}) => {
-    if (value.title !== undefined && value.description !== undefined) {
-      this.setState({
-        annotation,
-        data: {
-          title: value.title,
-          description: value.description,
-          id: Math.random() * 1000,
-        },
-      });
-    } else {
-      this.setState({ annotation });
-    }
-  };
-
-  onSubmit = () => {
-    const { annotation, annotations, data } = this.state;
-    this.setState({
-      annotation: {},
-      annotations: annotations.concat({
-        geometry: annotation.geometry,
-        data: { ...data },
-      }),
-    });
-
-    this.props.onSave({
-      data: annotation.geometry,
-      title: data.title,
-      description: data.description,
-    });
-  };
-
+  
   render() {
     return (
       <div className="App">
         <Annotation
           src={this.props.screenshot.src}
           alt={this.props.screenshot.alt}
-          annotations={this.state.annotations}
-          type={this.state.type}
-          value={this.state.annotation}
-          onChange={this.onChange}
+          annotations={this.props.annotations}
+          type={this.props.type}
+          value={this.props.annotation}
+          onChange={this.props.onChange}
           renderContent={({ annotation }) => {
             const { geometry } = annotation;
             return (
@@ -73,11 +36,11 @@ class CustomAnnotation extends Component {
           }}
           renderEditor={() => (
             <EditorWrapper
-              onChange={this.onChange}
-              annotation={this.state.annotation}
+              onChange={this.props.onChange}
+              annotation={this.props.annotation}
               description={this.description}
               title={this.title}
-              onSubmit={this.onSubmit}
+              onSubmit={this.props.onSubmit}
             />
           )}
         />
