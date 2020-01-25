915a06ffa1c1b50e4a72bf0a38a29def8914859c
Girmay <ink423103@yahoo.com>
2020-01-25 15:10:29 +0100

    Annotation functionality added

--- src/client/pages/Projects/ProjectsPage.js
+++ src/client/pages/Projects/ProjectsPage.js
@@ -9,62 +9,56 @@ import samplePhoto from '../../assets/images/profile.png';
 import './ProjectsPage.css';
 import MenuButton from '../../components/MenuButton/MenuButton.component';
 import MessageParagraph from '../../components/MessageParagraph/MessageParagraph.component';
-import BlogCardList from '../../components/ListOfBlogCard/ListOfBlogCard.component';
+import BlogCardList from '../../container/BlogCardList/BlogCardList.component';
 import { Consumer } from '../../context/AuthContext';
 import Loading from '../../components/Loading/Loading.component';
-//import AnnotationWrapper from '../../components/Annotation/AnnotationWrapper';
 import HomePageImage from '../../assets/images/HomePageImage.png';
-//import AuthContext from '../../context/AuthContext';
 import CustomAnnotation from '../../components/Annotation/CustomAnnotation.component';
+import AnnotationWrapper from '../../components/Annotation/AnnotationWrapper';
 
 class ProjectPage extends Component {
-  //static contextType = AuthContext;
   constructor() {
     super();
     this.state = {
       user: null,
-      annotations: null,
-      //screenshot_key: this.context,
       screenshotImage: '',
-     
+      annotations: [],
+      annotation: {},
+      data: {},
     };
   }
-  componentDidMount = ()=>{
-    //console.log('didmount function',this.context);
-    setInterval(() => {
-      const screenshotImage =  this.getSnapshotImage('j0vaxc');
-      if(screenshotImage)
-        this.setState({screenshotImage});
-    }, 1000);
-  }
-  getSnapshotImage = (key) =>{
-    fetch( `https://annotatetheweb.z16.web.core.windows.net/${key}/screenshot.jpg`)
-      .then(res =>res.json())
-      .then(json => {
-        console.log(json.url);
-        this.setState({screenshotImage: json.url });
-    })
-  }
-  onSave = (data, title, description, screenshotId) => {
-    fetch('http://localhost:3000/api/annotations/', {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        area: data,
-        fk_screenshot_id: screenshotId,
+  onChange = (annotation, value = {}) => {
+    if (value.title !== undefined && value.description !== undefined) {
+      this.setState({
+        annotation,
+        data: {
+          title: value.title,
+          description: value.description,
+          id: Math.random() * 1000,
+        },
+      });
+    } else {
+      this.setState({ annotation });
+    }
+  };
+  onSubmit = () => {
+    const { annotation, annotations, data } = this.state;
+    this.setState({
+      annotation: {},
+      annotations: annotations.concat({
+        geometry: annotation.geometry,
+        data: { ...data },
       }),
-    }).catch((error) => {
-      throw error;
+    });
+
+    this.props.onSave({
+      data: annotation.geometry,
+      title: data.title,
+      description: data.description,
     });
   };
-  
+
+
   render(){
     const profile = {
       src: samplePhoto,
@@ -77,66 +71,74 @@ class ProjectPage extends Component {
     };
     //const screenshotImage= this.props;
     return (
-      <div>
-      <div>
-          {this.state.screenshotImage&&<Loading />}
-        </div>
-      {!this.state.screenshotImage&&    
-      <div>
-        <div className="project-page-container">
-        <SidebarMenu />
-        <div className="mobilemenu-menuwrap">
-          <div className="header-mobile">
-            <Header title ={headerTitle}/>
-          </div>
-          <MenuButton />
-        </div>
-        <div className ="screenshot-image-container">
-          <CustomAnnotation
-            screenshot={backgroundImage}
-            onSave={({ data, title, description }) =>
-              onSave(data, title, description, props.screenshotId)
-            }
-          />
-        </div>
-
-        <div className="project-page-rightside-container">
-          <div className="project-page-rightside-innerContainer">
+      <Consumer>
+        {({ isAuthenticated, user_id, screenshot_key, setScreenshotKey }) => {
+          return(
             <div>
-              <Header title ={headerTitle}/>
-            </div>
-            {!this.state.user?
-              <div className="login-block">
-                <div >
-                  <Button buttonClassName="project-page-button" title="Login"/>
+            <div>
+                {this.state.screenshotImage&&<Loading />}
+              </div>
+            {!this.state.screenshotImage&&
+            <div>
+              <div className="project-page-container">
+              <SidebarMenu />
+              <div className="mobilemenu-menuwrap">
+                <div className="header-mobile">
+                  <Header title ={headerTitle}/>
                 </div>
-                <div >
-                  <RegisterButton registerButtonTitle="Register" />
+                <MenuButton />
+              </div>
+              <div className="screenshot-image-container">
+                <AnnotationWrapper screenshotId={1}
+                screenshotURL ="https://annotatetheweb.z16.web.core.windows.net/1l2w5s/screenshot.jpg"
+                onChange ={this.onChange}
+                onSubmit ={this.onSubmit}
+                type={this.state.type}
+                annotations={this.state.annotations}
+                annotation={this.state.annotation}
+                data={this.state.data}
+                />
+              </div>
+              <div className="project-page-rightside-container">
+                <div className="project-page-rightside-innerContainer">
+                  <div>
+                    <Header title ={headerTitle}/>
+                  </div>
+                  {this.state.user?
+                    <div className="login-block">
+                      <div >
+                        <Button buttonClassName="project-page-button" title="Login"/>
+                      </div>
+                      <div >
+                        <RegisterButton registerButtonTitle="Register" />
+                      </div>
+                    </div>
+                  :
+                    <div className="profile-container">
+                      <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+                    </div>
+                    }
+                  </div>
+                  {this.state.annotations ?
+                    <BlogCardList annotations={this.state.annotations}/>
+                   :
+                    <MessageParagraph className ="no-comment-message"
+                      message="For now you do not have any annotations:)"/>
+                    }
                 </div>
               </div>
-            :
-              <div className="profile-container">
-                <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+              <div className="project-page-footer">
+                <Footer />
               </div>
-              }
             </div>
-            {this.state.annotations?
-            <div className="comment-container">
-              <BlogCardList annotations={this.state.annotations}/>
-            </div>:
-              <MessageParagraph className ="no-comment-message"
-                message="For now you do not have any annotations:)"/>
             }
-          </div>
-        </div>
-        <div className="project-page-footer">
-          <Footer />
-        </div>
-      </div>
-  }
-      </div>  
+            </div>
+          )
+        }
+      }
+      </Consumer>
     );
-  }   
+  }
 };
 
 export default ProjectPage;
