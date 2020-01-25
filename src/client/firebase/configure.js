35af73c8a769241fd7c55e5f18f404568f209760
Laila1222 <korosililla@gmail.com>
2020-01-26 00:04:57 +0100

    set back firebase config

--- src/client/firebase/configure.js
+++ src/client/firebase/configure.js
@@ -3,7 +3,7 @@ import 'firebase/auth';
 
 export const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
