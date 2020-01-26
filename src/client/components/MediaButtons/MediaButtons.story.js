8ed161f861d0ec816355c82b77b861b0cd18e030
Fatemeh Pakpour <fatemehpakpour@MacBook-Pro-3.localdomain>
2020-01-26 17:22:34 +0100

    stylecomponent

--- src/client/components/MediaButtons/MediaButtons.story.js
+++ src/client/components/MediaButtons/MediaButtons.story.js
@@ -5,6 +5,8 @@ import MediaButtons from './MediaButtons.component';
 
 storiesOf('MediaButtons', module)
   .add('MediaButtons', () => (
-    <MediaButtons url="http://localhost:3007/?path=/story/mediabuttons--mediabuttons" title="annotation"/>
+    <div>
+      <MediaButtons url="http://localhost:3007/?path=/story/mediabuttons--mediabuttons" title="annotation"/>
+    </div>
   ));
 export default MediaButtons;  
