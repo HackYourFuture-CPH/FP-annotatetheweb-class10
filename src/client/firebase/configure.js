89ec3566648603b844fe293f6b90001a66955f2d
Laila1222 <korosililla@gmail.com>
2020-01-27 22:06:14 +0100

    added word

--- src/client/firebase/configure.js
+++ src/client/firebase/configure.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
