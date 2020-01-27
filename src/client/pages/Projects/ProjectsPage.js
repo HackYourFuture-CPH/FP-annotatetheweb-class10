0a23c1ce701bc9b323c298fc6969277cbd509f4e
Ida <ida.naderian@gmail.com>
2020-01-27 06:18:05 +0100

    get screenshotURL from api.

--- src/client/pages/Projects/ProjectsPage.js
+++ src/client/pages/Projects/ProjectsPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import SidebarMenu from '../../components/SidebarMenu/SidebarMenu.component';
 import ProfileSummery from '../../components/ProfileSummary/ProfileSummary.component';
 import Header from '../../components/Header/Header.Component';
@@ -16,56 +16,49 @@ import HomePageImage from '../../assets/images/HomePageImage.png';
 import AnnotationWrapper from '../../components/Annotation/AnnotationWrapper';
 
 class ProjectPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-      screenshotImage: '',
-      annotations: [],
-      annotation: {},
-      data: {},
-    };
+  state = {
+    user: null,
+    screenshotImage: '',
+    screenshotsKey: ' ',
+  };
+
+  componentDidMount() {
+    const screenshotsKey =
+      JSON.parse(localStorage.getItem('screenshot_key')) || [];
+    // eslint-disable-next-line react/no-unused-state
+    this.setState({ screenshotsKey });
   }
 
-  onChange = (annotation, value = {}) => {
-    if (value.title !== undefined && value.description !== undefined) {
-      this.setState({
-        annotation,
-        data: {
-          title: value.title,
-          description: value.description,
-          id: Math.random() * 1000,
+  // eslint-disable-next-line react/sort-comp
+  getScreenshotURL = () => {
+    fetch(
+      `https://api.annotatetheweb.com/screenshot/1.0/GetScreenshot?key=${this.state.screenshotsKey}`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
         },
+      },
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.url;
+      })
+      .catch((error) => {
+        throw error;
       });
-    } else {
-      this.setState({ annotation });
-    }
   };
 
-  onSubmit = () => {
-    const { annotation, annotations, data } = this.state;
-    this.setState({
-      annotation: {},
-      annotations: annotations.concat({
-        geometry: annotation.geometry,
-        data: { ...data },
-      }),
-    });
-
-    this.props.onSave({
-      data: annotation.geometry,
-      title: data.title,
-      description: data.description,
-    });
-  };
-
-
-  render(){
+  render() {
     const profile = {
       src: samplePhoto,
       alt: 'profile image',
     };
 
+    console.log(this.getScreenshotURL(this.state.screenshotsKey));
     const headerTitle = 'Annotate the web';
     // eslint-disable-next-line no-unused-vars
     const backgroundImage = {
@@ -75,66 +68,66 @@ class ProjectPage extends Component {
     return (
       <Consumer>
         {() => {
-          return(
+          return (
             <div>
-            <div>
-                {this.state.screenshotImage&&<Loading />}
-              </div>
-            {!this.state.screenshotImage&&
-            <div>
-              <div className="project-page-container">
-              <SidebarMenu />
-              <div className="screenshot-image-container">
-                <AnnotationWrapper screenshotId={1}
-                screenshotURL ="https://annotatetheweb.z16.web.core.windows.net/1l2w5s/screenshot.jpg"
-                onChange ={this.onChange}
-                onSubmit ={this.onSubmit}
-                type={this.state.type}
-                annotations={this.state.annotations}
-                annotation={this.state.annotation}
-                data={this.state.data}
-                />
-              </div>
-              <div className="project-page-rightside-container">
-                <div className="project-page-rightside-innerContainer">
-                  <div>
-                    <Header title ={headerTitle}/>
-                  </div>
-                  {this.state.user?
-                    <div className="login-block">
-                      <div >
-                        <Button buttonClassName="project-page-button" title="Login"/>
-                      </div>
-                      <div >
-                        <RegisterButton registerButtonTitle="Register" />
-                      </div>
+              <div>{this.state.screenshotImage && <Loading />}</div>
+              {!this.state.screenshotImage && (
+                <div>
+                  <div className="project-page-container">
+                    <SidebarMenu />
+                    <div className="screenshot-image-container">
+                      <AnnotationWrapper
+                        screenshotId={19}
+                        screenshotURL={this.getScreenshotURL()}
+                      />
                     </div>
-                  :
-                    <div className="profile-container">
-                      <ProfileSummery profileName="Kseiina Zar" profileImage={profile}/>
+                    <div className="project-page-rightside-container">
+                      <div className="project-page-rightside-innerContainer">
+                        <div>
+                          <Header title={headerTitle} />
+                        </div>
+                        {this.state.user ? (
+                          <div className="login-block">
+                            <div>
+                              <Button
+                                buttonClassName="project-page-button"
+                                title="Login"
+                              />
+                            </div>
+                            <div>
+                              <RegisterButton registerButtonTitle="Register" />
+                            </div>
+                          </div>
+                        ) : (
+                          <div className="profile-container">
+                            <ProfileSummery
+                              profileName="Kseiina Zar"
+                              profileImage={profile}
+                            />
+                          </div>
+                        )}
+                      </div>
+                      {this.state.annotations ? (
+                        <BlogCardList annotations={this.state.annotations} />
+                      ) : (
+                        <MessageParagraph
+                          className="no-comment-message"
+                          message="For now you do not have any annotations:)"
+                        />
+                      )}
                     </div>
-                    }
                   </div>
-                  {this.state.annotations ?
-                    <BlogCardList annotations={this.state.annotations}/>
-                   :
-                    <MessageParagraph className ="no-comment-message"
-                      message="For now you do not have any annotations:)"/>
-                    }
+                  <div className="project-page-footer">
+                    <Footer />
+                  </div>
                 </div>
-              </div>
-              <div className="project-page-footer">
-                <Footer />
-              </div>
+              )}
             </div>
-            }
-            </div>
-          )
-        }
-      }
+          );
+        }}
       </Consumer>
     );
   }
-};
+}
 
 export default ProjectPage;
