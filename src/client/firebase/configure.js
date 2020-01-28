a6c2efcc17bcfe11f3adff15840002bf9fd76e43
Niels Schmidt <niels@bytewiz.io>
2020-01-29 00:48:56 +0100

    moved firebase config vars

--- src/client/firebase/configure.js
+++ src/client/firebase/configure.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyAROMf-XVOoPXN_idZbYigkdn0eABbQbjA',
+  authDomain: 'fp-annotatetheweb-class10.firebaseapp.com',
+  databaseURL: 'https://fp-annotatetheweb-class10.firebaseio.com',
+  projectId: 'fp-annotatetheweb-class10',
+  storageBucket: 'fp-annotatetheweb-class10.appspot.com',
+  messagingSenderId: '845246565627',
+  appId: '1:845246565627:web:714afab19ea6670695745e',
+  measurementId: 'G-R64ZFS2G88',
 };
 // eslint-disable-next-line
 let app = null;
