47964cfbae9390eb5e1e9bfbd3236d88d445681c
Olha Afanasieva <afanasievaolja@gmail.com>
2020-02-05 23:47:40 +0100

    fix migration error

--- src/server/migrations/20200205234609_adding_uid_to_users.js
+++ src/server/migrations/20200205234609_adding_uid_to_users.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
     return knex.schema.table('users', function(table) {
-      table.string('email').notNull();
+      table.string('uid').notNull();
     });
   };
   
   exports.down = function(knex) {
     return knex.schema.table('users', function(table) {
-      table.dropColumn('email');
+      table.dropColumn('uid');
     });
   };
--- src/server/seeds/002_00_users.js
+++ src/server/seeds/002_00_users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex('users').insert([
-        { user_id: 1, name: 'John Doe',  uid: 'DFDSIFHDKFHDSK73857934', fk_role_id: 1, user_name: 'JD' },
+        { user_id: 1, name: 'John Doe',  uid: 'DFDSIFHDKFHDSK7385793485', fk_role_id: 1, user_name: 'JD' },
       ]);
     });
 };
