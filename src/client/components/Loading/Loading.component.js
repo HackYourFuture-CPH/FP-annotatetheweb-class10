4f242fadd96dafbfbe21a53c30641bfc00fc9bda
Olha Afanasieva <afanasievaolja@gmail.com>
2020-02-12 17:16:51 +0100

    fix login component

--- src/client/components/Loading/Loading.component.js
+++ src/client/components/Loading/Loading.component.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 export default function Loading() {
   return (
-    <div class="lds-ring">
-      <div>-Loading</div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="lds-ring">
+      <div>Loading</div>
     </div>
   );
 }
